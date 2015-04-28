ee833265fbf58168930dffff9d3ea0963616191a
CurosMJ <curosmj@gmail.com>
2015-04-28 19:37:17 +0530

    pages loaded according to the request path about us page content updated added requirements file

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from flask import *
+from os.path import isfile
+from os import getcwd
 
 app = Flask(__name__)
 
@@ -6,9 +8,12 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/<pagename>')
+def page(pagename):
+    if isfile(getcwd() + '/templates/' + pagename + '.html' ) :
+        return render_template(pagename + '.html')
+    else:
+        abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
